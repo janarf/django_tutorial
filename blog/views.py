cfaa88bfe9b5809c435b909b11fddce6de5859ac
janarf <janarf@hotmail.com>
2019-05-03 13:22:56 -0300

    django project tutorial

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Post
 
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context={
+        'posts': Post.objects.all()
+    }
+    return render (request, 'blog/home.html',context)
 
 def about(request):
-    return HttpResponse('<h1>Blog about</h1>')
+
+    return render(request, 'blog/about.html',{'title':'About'})
